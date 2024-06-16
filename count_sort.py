495fe80e9d248670deada4fda013a378a770359b
dheeresh-attentive <dheeresh@attentive.ai>
2024-06-17 05:17:41 +0530

    count sort

---
+++ count_sort.py
@@ -0,0 +1,21 @@
+def count_sort(arr):
+    mx = max(arr)
+    n = len(arr)
+    output = [0] * n
+    count = [0] * (mx + 1)
+
+    for i in range(n):
+        count[arr[i]] += 1
+
+    for i in range(1, mx + 1):
+        count[i] += count[i - 1]
+
+    for i in range(n - 1, -1, -1):
+        count[arr[i]] -= 1
+        output[count[arr[i]]] = arr[i]
+
+    return output
+
+
+arr = [1, 2, 4, 3, 5, 6, 7, 8, 9, 10]
+print(count_sort(arr))
