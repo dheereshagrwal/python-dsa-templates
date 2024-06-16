c9883a3a08ca57447fdd71f33bcea2f33f314d47
dheeresh-attentive <dheeresh@attentive.ai>
2024-06-17 03:30:52 +0530

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+def bubble_sort_optimized(arr):
+    n = len(arr)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(n - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+    return arr
+
+
+arr = [1, 2, 4, 3, 5, 6, 7, 8, 9, 10]
+print(bubble_sort_optimized(arr))
