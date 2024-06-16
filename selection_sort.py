7b7634a2a892a2cee4e7a9fbf86c9d647774ff88
dheeresh-attentive <dheeresh@attentive.ai>
2024-06-17 04:17:47 +0530

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+arr = [1, 2, 4, 3, 5, 6, 7, 8, 9, 10]
+print(selection_sort(arr))
