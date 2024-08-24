20607041652e5227b093fd596e1d28ad127d7fbb
dheereshagrwal <da11@iitbbs.ac.in>
2024-08-24 06:17:25 +0530

    binary search

--- binary_search.py
+++ binary_search.py
@@ -13,19 +13,3 @@ def binary_search(arr, x):
         else:
             high = mid - 1
     return -1
-
-
-def binary_search(arr, x, low, high):
-    """
-    Search for x in the sorted list arr using binary search algorithm
-    Returns the index of x in arr if found, otherwise returns -1
-    """
-    while low <= high:
-        mid = (low + high) >> 1
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] < x:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return -1
